d627ed76f02bd09d8d8d8334e1659e09621fe79a
刘群 <liuqun@usmaker.cn>
2017-04-14 11:36:11 +0800

    代码是Copy的,是一个类似思维导图的ViewGroup

---
+++ app/src/main/java/com/owant/thinkmap/test/TestActivity.java
@@ -0,0 +1,39 @@
+package com.owant.thinkmap.test;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.owant.thinkmap.R;
+import com.owant.thinkmap.base.BaseActivity;
+
+/**
+ * Created by owant on 08/03/2017.
+ */
+
+public class TestActivity extends BaseActivity {
+
+    @Override
+    protected void onBaseIntent() {
+
+    }
+
+    @Override
+    protected void onBasePreLayout() {
+
+    }
+
+    @Override
+    protected int onBaseLayoutId(@Nullable Bundle savedInstanceState) {
+        return R.layout.test_gv;
+    }
+
+    @Override
+    protected void onBaseBindView() {
+
+    }
+
+    @Override
+    protected void onLoadData() {
+
+    }
+}
